2fcb53f679359eed5496fe59534f722cfcc32184
NoorTushar <tushar.noor.khan@gmail.com>
2024-01-30 17:51:57 +0600

    m

---
+++ index.js
@@ -0,0 +1,25 @@
+// write a program to find the remainder of two numbers
+
+/*let input = "30 20";
+let data = input.split(" ");
+
+let num1 = Number(data[0]);
+
+let num2 = Number(data[1]);
+
+let remainder = num1 % num2;
+console.log(remainder);*/
+
+/*
+Write a program to find the quotient of two numbers (integer division).
+*/
+
+let input = "45 35";
+let data = input.split(" ");
+
+let num1 = Number(data[0]);
+
+let num2 = Number(data[1]);
+
+let quotient = parseInt(num1 / num2);
+console.log(quotient);
