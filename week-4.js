8e701ee3fc924e70aad7ad1fe6a964498bc6c818
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-13 20:26:59 +0600

    week 4

---
+++ week-4.js
@@ -0,0 +1,34 @@
+/**
+ * Problem Statement
+Write a program to check if there is a vowel in a string.
+
+Input
+The input consists of a string.
+
+Output
+Output will be the answer whether there is a vowel or not. If there is vowel it will print "The string contains a vowel.", otherwise it will print "The string does not contain any vowel."
+
+Constraints
+The program will terminate whenever it finds a vowel.
+ */
+
+// let input = "hello";
+// const vowels = ["a", "e", "i", "o", "u"];
+
+// let sentence = input;
+
+// sentence = sentence.toLowerCase();
+
+// let sentenceArray = sentence.split();
+
+// let result = "";
+// for (let letter of sentence) {
+//    if (vowels.includes(letter)) {
+//       result = `The string contains a vowel.`;
+//       break;
+//    } else {
+//       result = `The string does not contain any vowel`;
+//    }
+// }
+
+// console.log(result);
