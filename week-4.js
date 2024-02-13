0ac2cddef9d58d5192ba697dd9b794caab7bf0dd
NoorTushar <tushar.noor.khan@gmail.com>
2024-02-13 21:27:27 +0600

    week 4

--- week-4.js
+++ week-4.js
@@ -64,3 +64,52 @@ Output:
 // const nextNumber = Number(numbers[numbers.length - 1]) + difference;
 
 // console.log(nextNumber);
+
+/**
+ * BMI Calculator
+বাং
+
+Problem Statement
+Write a program where user will give height(height) and weight(kg) and then BMI will be calculated.
+
+Input
+The input consists of two double numbers which are height(meter) and weight(kg).
+
+Output
+*if bmi < 18.5 print "Underweight" *if bmi >= 18.5 & bmi < 25.0 print "Normal weight" *if bmi >= 25.0 & bmi < 30.0 print "Overweight" *else print "Obese"
+
+Constraints
+0 ≤ |S| ≤ 109
+Example:
+Enter height and weight.
+
+Input:
+1.6 60
+Output:
+BMI: 23.44
+Normal weight
+ */
+
+let input = "1.6 60";
+const variables = input.split(" ");
+
+const height = parseFloat(variables[0]);
+const weight = parseFloat(variables[1]);
+
+const bmi = (weight / Math.pow(height, 2)).toFixed(2);
+
+let statement = "";
+
+if (bmi < 18.5) {
+   statement = "Underweight";
+} else if (bmi >= 18.5 && bmi < 25.0) {
+   statement = "Normal weight";
+} else if ((bmi >= 25.0) & (bmi < 30.0)) {
+   statement = "Overweight";
+} else {
+   statement = "Obese";
+}
+
+console.log(`BMI: ${bmi}
+${statement}
+`);
